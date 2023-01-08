5160fe875edd4f23b6c8567a1ef4ba57bf4560bb
omid-aram <omid.aram@hotmail.com>
2023-01-08 15:54:07 +0330

    adding store.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { StoreProvider } from 'easy-peasy';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </Router>
+    <StoreProvider store={store}>
+      <Router>
+        <Routes>
+          <Route path="*" element={<App />} />
+        </Routes>
+      </Router>
+    </StoreProvider>
   </React.StrictMode>
 );
